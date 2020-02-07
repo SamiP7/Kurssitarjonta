9ea19d9b203456a1d1d4535abdd611950e3ede79
Sami <sami.parkkinenn@gmail.com>
2020-02-07 16:38:08 +0200

    puhelinnumero ilmoitettu vapaavalintaiseksi

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -5,7 +5,7 @@ class LoginForm(FlaskForm):
 	email = StringField("Email", [validators.Email(message="Not a valid email")])
 	password = PasswordField("Password", [validators.Length(min=5)])
 	name = StringField("Name", [validators.Length(min=1)])
-	phonenumber = StringField("Phone number")
+	phonenumber = StringField("Phone number(optional)")
 	
 	class Meta:
 		csrf = False
